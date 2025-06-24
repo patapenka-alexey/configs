86cbcae5272d453869629b4a9f0331b65ab453c2
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-06-24 16:51:06 +0300

    cursors: add cz-Viator-Rotor

--- configs/cursors/uninstall.sh
+++ configs/cursors/uninstall.sh
@@ -3,4 +3,5 @@
 sudo rm -rf /usr/share/icons/mac-cursors/
 sudo rm -rf /usr/share/icons/material_light_cursors/
 sudo rm -rf /usr/share/icons/We10XOS-cursors/
+sudo rm -rf /usr/share/icons/cz-Viator-Rotor/
 
