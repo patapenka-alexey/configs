feffc922158c9b96bb9b4b3f62afcce05a2af5ee
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-06-25 10:01:57 +0300

    cursors: added neutrality (black pixels)

--- configs/cursors/uninstall.sh
+++ configs/cursors/uninstall.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+sudo rm -rf /usr/share/icons/cz-Viator-Rotor/
 sudo rm -rf /usr/share/icons/mac-cursors/
 sudo rm -rf /usr/share/icons/material_light_cursors/
 sudo rm -rf /usr/share/icons/We10XOS-cursors/
-sudo rm -rf /usr/share/icons/cz-Viator-Rotor/
+sudo rm -rf /usr/share/icons/neutrality-2024-04-13/
 
