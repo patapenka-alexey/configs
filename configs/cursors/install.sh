c357fbf8fc4fcd9c60504fab13b37f33e520db79
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-06-26 16:06:53 +0300

    cursors: add installation command without UI

--- configs/cursors/install.sh
+++ configs/cursors/install.sh
@@ -6,3 +6,9 @@ sudo cp -r $SCRIPT_DIR /usr/share/icons/
 
 sudo update-icon-caches /usr/share/icons/*
 
+
+#sudo update-alternatives --install x-cursor-theme \
+#    /usr/share/icons/mac-cursors/cursor.theme
+
+#sudo update-alternatives --auto x-cursor-theme
+
--- configs/cursors/uninstall.sh
+++ configs/cursors/uninstall.sh
@@ -2,7 +2,9 @@
 
 sudo rm -rf /usr/share/icons/cz-Viator-Rotor/
 sudo rm -rf /usr/share/icons/mac-cursors/
-sudo rm -rf /usr/share/icons/material_light_cursors/
-sudo rm -rf /usr/share/icons/We10XOS-cursors/
 sudo rm -rf /usr/share/icons/neutrality-2024-04-13/
+sudo rm -rf /usr/share/icons/We10XOS-cursors/
+
+# sudo update-alternatives --remove x-cursor-theme \
+#     /usr/share/icons/mac-cursors/cursor.theme
 
