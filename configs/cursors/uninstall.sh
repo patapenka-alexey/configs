5e328a791ae771c923e41e649be8f9e7b40afc20
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-06-26 16:14:00 +0300

    cursors: remove unused, add new in archives

--- configs/cursors/uninstall.sh
+++ configs/cursors/uninstall.sh
@@ -2,8 +2,6 @@
 
 sudo rm -rf /usr/share/icons/cz-Viator-Rotor/
 sudo rm -rf /usr/share/icons/mac-cursors/
-sudo rm -rf /usr/share/icons/neutrality-2024-04-13/
-sudo rm -rf /usr/share/icons/We10XOS-cursors/
 
 # sudo update-alternatives --remove x-cursor-theme \
 #     /usr/share/icons/mac-cursors/cursor.theme
