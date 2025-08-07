9e957d66fd9be0a9bf6eea1513c1a51a4067031b
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-08-07 14:58:55 +0300

    git: some useful aliases

---
+++ scripts/git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+alias gd='git diff'
+alias gds='git diff --staged'
+
+alias gs='git status'
+
+alias ga='git add'
+
+alias gcm='git commit -m'
+alias gca='git commit --amend'
+
+alias gch='git checkout'
+alias gchm='git checkout master'
+
+alias gb='git branch'
+# view remote branches
+alias gbr='git branch --remote'
+
+alias gplr='git pull --rebase'
+
+alias gp='git push'
+alias gpf='git push --force'
+
